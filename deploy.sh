7f8976579f4e02fe3aade3a55714a297c553fedc
ZeusTheTrueGod <l-2732@yandex.ru>
2021-10-20 16:23:43 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh -t root@86.109.11.205 <<'EOL'
-  bash -lc 'cd web-landscape && git pull && pm2 status && pm2 stop all && pm2 start'
+  tmux -c 'cd web-landscape && git pull && pm2 status && pm2 stop all && pm2 start'
 EOL
 
