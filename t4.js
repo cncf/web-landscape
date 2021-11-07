fd668e88f858851b7b22cfdd09a72edc7498967a
ZeusTheTrueGod <l-2732@yandex.ru>
2021-11-07 09:10:01 +0000

    more progress

--- t4.js
+++ t4.js
@@ -1,8 +1,4 @@
 const client = require('@octokit/core').Octokit;
-const octokit = new client({
-        auth: '670ee0953a3a3ca443781c9fe4a2aa312bd83c0b'
-});
-
 async function main() {
     const owner = 'lfai';
     const repo = 'lfai-landscape';
