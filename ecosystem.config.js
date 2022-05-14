58863b7fc6c6eb32729defc19511aa8e7e64825e
Andrey Kozlov <zeus@Andreys-MacBook-Pro.local>
2022-05-14 19:12:10 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
       name: "server",
-      script: ". ~/landscapes.env && node ./server.js",
+      script: ". ~/landscapes.env && yarn node ./server.js",
       watch: true,
       cwd: require('path').resolve(__dirname)
   }]
