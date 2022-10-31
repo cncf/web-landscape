b7abd96c24cae02e1fedea039ce1caf7d329eb13
Andrey Kozlov <zeus@Andreys-MacBook-Pro.local>
2022-10-31 21:40:10 +0000

    fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.KEY2) {
         process.env.KEY2.replaceAll(" ","\n") + 
         "\n-----END OPENSSH PRIVATE KEY-----\n"
     );
-    require('fs').chmodSync(process.env.HOME + '/.ssh/bot2', 0o400);
+    require('fs').chmodSync(process.env.HOME + '/.ssh/bot2', 0o600);
 }
 
 
